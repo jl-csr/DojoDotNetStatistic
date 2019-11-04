784548d6fa26302979ee22fc2405e456dd41843a
Julio Cesar <jcltamogami@gmail.com>
2019-11-04 18:04:31 -0300

    Finishing dojo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DojoDotNetStatistic
 {
@@ -6,9 +8,59 @@ namespace DojoDotNetStatistic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your favorite numbers separated by \",\":");
+
+            var numbers = Console.ReadLine();
+            var arrayOfNumbers = GetArray(numbers);
+
+            if(arrayOfNumbers == null || !arrayOfNumbers.Any())
+                return;
+
+            arrayOfNumbers = arrayOfNumbers.OrderBy(o => o);
+
+            Console.WriteLine("Results:");
+            Console.WriteLine($"Minimum value: {arrayOfNumbers.FirstOrDefault()}");
+            Console.WriteLine($"Maximum value: {arrayOfNumbers.LastOrDefault()}");
+            Console.WriteLine($"Average: {arrayOfNumbers.Average()}");
+            Console.WriteLine($"Total of numbers: {arrayOfNumbers.Count()}");
 
             Console.ReadKey();
+        }
+
+        public static IEnumerable<int> GetArray(string sentence)
+        {
+            var arrayOfNumbers = new List<int>();
+
+            sentence.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList()
+                    .ForEach(n => {
+
+                        int converted;
+
+                        if (!int.TryParse(n, out converted))
+                        {
+                            ShowError("Your sentence contains a non-numeric digit.");
+
+                            arrayOfNumbers = null;
+
+                            return;
+                        }
+
+                        arrayOfNumbers.Add(converted);
+                    });
+
+            return arrayOfNumbers;
+        }
+
+        public static void ShowError(string message)
+        {
+            var currentColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine($"Error: {message}");
+
+            Console.ForegroundColor = currentColor;
+
         }
     }
 }
